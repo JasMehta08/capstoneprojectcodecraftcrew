76f0b976816be0c7779c02f13f0f32455b8ef98a
JasMehta08 <jasmehta805@gmail.com>
2024-04-05 17:17:59 +0530

    added way to display the whole club

--- club/clubtypesearch.cpp
+++ club/clubtypesearch.cpp
@@ -13,6 +13,8 @@
  
 using namespace std;
 
+
+
 // basic details of a particular member
 class details{
     private: 
@@ -54,9 +56,133 @@ class details{
         cout<< endl << "Faculty(True / False): ";
         cout << faculty<<"\n"<<endl; 
     }
+
+    void print_club(){
+        cout <<' ' << st_id <<' ' << "      "<<"  "<<faculty<<' '<<"        "<<' '<< st_name<<' '<<endl;
+    }
 };
 
-// general list consisting of all the members of all the clubs
+
+// details of a particular club will be stored in this class
+class club {
+    private :
+    int n ;
+    vector < details > list_of_members ;
+    public: 
+    club(){
+        n = 0;
+    }
+    void add_member_to_club(details member){
+        list_of_members.push_back(member);
+        n++;
+    }
+    void printing_club(string club){
+        cout<< "the list of the members of the club : "<<club<<endl<<endl;
+        cout << " Student ID " << "      "<<" Faculty "<<"      "<<" Student Name "<<endl<<endl;
+        for(int i =0 ; i< list_of_members.size(); i++ ){
+            details temp_mem = list_of_members[i]; 
+            temp_mem.print_club();
+        }
+    }
+};
+
+// details of same type of clubs are stored here
+class clubtype{
+    private: 
+    vector < club > list_of_clubs;
+    int m ;
+    public: 
+    clubtype(){
+        m=0;
+    }
+    void add_club(club add){
+        list_of_clubs.push_back(add);
+        m++;
+    }
+    
+   
+};
+
+club Khelaiya_club,Press_club,Debate_club,Music_club,Dance_club,Cubing_club,Quiz_club,Drama_club,Muse_club,Radio_club,Programming_club,Developers_club;
+
+void sort_into_club(details member,string club){
+            if(club == "Khelaiya_club"){
+                 Khelaiya_club.add_member_to_club(member);
+            }
+            else if(club == "Press_club"){
+                 Press_club.add_member_to_club(member);
+            }
+            else if(club == "Music_club"){
+                 Music_club.add_member_to_club(member);
+            }
+            else if(club == "Debate_club"){
+                 Debate_club.add_member_to_club(member);
+            }
+            else if(club == "Dance_club"){
+                 Dance_club.add_member_to_club(member);
+            }
+            else if(club == "Cubing_club"){
+                 Cubing_club.add_member_to_club(member);
+            }
+            else if(club == "Quiz_club"){
+                 Quiz_club.add_member_to_club(member);
+            }
+            else if(club == "Drama_club"){
+                 Drama_club.add_member_to_club(member);
+            }
+            else if(club == "Muse_club"){
+                 Muse_club.add_member_to_club(member);
+            }
+            else if(club == "Radio_club"){
+                 Radio_club.add_member_to_club(member);
+            }
+            else if(club == "Programming_club"){
+                 Programming_club.add_member_to_club(member);
+            }
+            else if(club == "Developers_club"){
+                 Developers_club.add_member_to_club(member);
+            }
+}
+void display_club(string club){
+            if(club == "Khelaiya club"){
+                 Khelaiya_club.printing_club(club);
+            }
+            else if(club == "Press club"){
+                 Press_club.printing_club(club);
+            }
+            else if(club == "Music club"){
+                 Music_club.printing_club(club);
+            }
+            else if(club == "Debate club"){
+                 Debate_club.printing_club(club);
+            }
+            else if(club == "Dance club"){
+                 Dance_club.printing_club(club);
+            }
+            else if(club == "Cubing club"){
+                 Cubing_club.printing_club(club);
+            }
+            else if(club == "Quiz club"){
+                 Quiz_club.printing_club(club);
+            }
+            else if(club == "Drama club"){
+                 Drama_club.printing_club(club);
+            }
+            else if(club == "Muse club"){
+                 Muse_club.printing_club(club);
+            }
+            else if(club == "Radio club"){
+                 Radio_club.printing_club(club);
+            }
+            else if(club == "Programming club"){
+                 Programming_club.printing_club(club);
+            }
+            else if(club == "Developers club"){
+                 Developers_club.printing_club(club);
+            }
+    }
+
+    // general list consisting of all the members of all the clubs
 class general_list{
     public: 
         unordered_map<int , details> gen_list;
@@ -91,42 +217,7 @@ class general_list{
                     member.create(stoi(id) , name , club , clubtype , faculty);
                     add_new(stoi(id), member);
                     add_2new(name,member);
-                if(club == "Khelaiya club"){
-                 Khelaiya_club.add_member_to_club(member);
-            }
-                if(club == "Press club"){
-                 Press_club.add_member_to_club(member);
-            }
-                if(club == "Music club"){
-                 Music_club.add_member_to_club(member);
-            }
-                if(club == "Debate club"){
-                 Debate_club.add_member_to_club(member);
-            }
-                if(club == "Dance club"){
-                 Dance_club.add_member_to_club(member);
-            }
-                if(club == "Cubing club"){
-                 Cubing_club.add_member_to_club(member);
-            }
-                if(club == "Quiz club"){
-                 Quiz_club.add_member_to_club(member);
-            }
-                if(club == "Drama club"){
-                 Drama_club.add_member_to_club(member);
-            }
-                if(club == "Muse club"){
-                 Muse_club.add_member_to_club(member);
-            }
-                if(club == "Radio club"){
-                 Radio_club.add_member_to_club(member);
-            }
-                if(club == "Programming club"){
-                 Programming_club.add_member_to_club(member);
-            }
-                if(club == "Developers club"){
-                 Developers_club.add_member_to_club(member);
-            }
+                sort_into_club(member,club);
                 
             }
             file.close();
@@ -144,38 +235,7 @@ class general_list{
             member.print();
         }
 };
-// details of a particular club will be stored in this class
-class club {
-    private :
-    int n ;
-    vector < details > list_of_members ;
-    public: 
-    club(){
-        n = 0;
-    }
-    void add_member_to_club(details member){
-        list_of_members.push_back(member);
-        n++;
-    }
-    
 
-};
-
-// details of same type of clubs are stored here
-class clubtype{
-    private: 
-    vector < club > list_of_clubs;
-    int m ;
-    public: 
-    clubtype(){
-        m=0;
-    }
-    void add_club(club add){
-        list_of_clubs.push_back(add);
-        m++;
-    }
-   
-};
 int main(){
     ofstream outputFile("output.txt");          // Opening a file for output
     
@@ -189,10 +249,10 @@ int main(){
 
     // Our main code
     general_list main ;
-    club Khelaiya_club,Press_club,Debate_club,Music_club,Dance_club,Cubing_club,Quiz_club,Drama_club,Muse_club,Radio_club,Programming_club,Developers_club;
     main.get_from_file("gen_list.txt");
     main.find_mem_by_id(202301414);
     main.find_mem_by_name("Parshv_Joshi"); 
+    display_club("Khelaiya club");
     // end of the main code
     
     cout.rdbuf(coutbuf);
